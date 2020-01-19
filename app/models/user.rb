4788ecb7cac84f0867de1a8191e30bd0863183dd
dcig <dancigler@gmail.com>
2020-01-18 20:54:30 -0600

    add association between user/article

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 105 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
+
+  has_many :articles
+  before_save { self.email = email.downcase }
 end
